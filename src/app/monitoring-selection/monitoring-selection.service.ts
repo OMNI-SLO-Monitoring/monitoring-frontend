fc920cc23659f181f9eaffdaca643feda0f60258
Jiaxuan He <jiaxuan_he@yahoo.de>
2020-07-28 16:12:28 +0200

    SLADIM-95 fixed tests

--- src/app/monitoring-selection/monitoring-selection.service.ts
+++ src/app/monitoring-selection/monitoring-selection.service.ts
@@ -2,43 +2,55 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 /**
  * Provides the methods for sending requests to the backend regarding
  * monitored services
  */
 export class MonitoringSelectionService {
-
   backendUrl = 'http://localhost:3400/monitoring-selection';
 
   // array of monitored services objects, is used to display monitored services
   selectedServices = [];
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   /**
    * Sends a request to the backend to add a service to the database
    *
    * @param selection Service added to be monitored
    */
-  addSelection(selection: any): Promise <any> {
-    return this.httpClient.post(this.backendUrl, selection).toPromise();
+  addSelection(selection: any): Promise<any> {
+    try {
+      return this.httpClient.post(this.backendUrl, selection).toPromise();
+    } catch (err) {
+      console.log('Backend not available');
+    }
   }
   /**
    * Sends a request to the backend to delete the selected service in the database
    *
    * @param selectionId Id of monitored Service
    */
-  deleteSelection(selectionId: string): Promise <any> {
-    return this.httpClient.delete(this.backendUrl + '/' + selectionId).toPromise();
+  deleteSelection(selectionId: string): Promise<any> {
+    try {
+      return this.httpClient
+        .delete(this.backendUrl + '/' + selectionId)
+        .toPromise();
+    } catch (err) {
+      console.log('Backend not available');
+    }
   }
 
   /**
    * Sends a request to the backend to get all services currently monitored
    */
-  getAllSelectedServices(): Observable <any> {
-    return this.httpClient.get(this.backendUrl);
+  getAllSelectedServices(): Observable<any> {
+    try {
+      return this.httpClient.get(this.backendUrl);
+    } catch (err) {
+      console.log('Backend not available');
+    }
   }
 }

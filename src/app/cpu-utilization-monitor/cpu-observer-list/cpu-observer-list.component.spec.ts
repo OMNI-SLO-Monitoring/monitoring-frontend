2c7a43b8a9acb04820c714a09fc111cc3031c39b
Jiaxuan He <jiaxuan_he@yahoo.de>
2020-08-02 15:56:56 +0200

    fixed tests

--- src/app/cpu-utilization-monitor/cpu-observer-list/cpu-observer-list.component.spec.ts
+++ src/app/cpu-utilization-monitor/cpu-observer-list/cpu-observer-list.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CpuObserverListComponent } from './cpu-observer-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule } from '@angular/material/dialog';
+import { EndpoitsService } from '../endpoits.service';
 
 describe('CpuObserverListComponent', () => {
   let component: CpuObserverListComponent;
@@ -12,6 +13,7 @@ describe('CpuObserverListComponent', () => {
     TestBed.configureTestingModule({
       declarations: [CpuObserverListComponent],
       imports: [HttpClientModule, MatDialogModule],
+      providers: [EndpoitsService],
     }).compileComponents();
   }));
 

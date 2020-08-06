5356a4612d6a32d65e892809da3a504b13ca5f14
Jonas Wallmann <jonaswallmann@hotmail.de>
2020-08-06 12:26:10 +0200

    minimum input functionality

--- src/app/cpu-utilization-monitor/endpoits.service.ts
+++ src/app/cpu-utilization-monitor/endpoits.service.ts
@@ -3,13 +3,13 @@ import { Observable } from 'rxjs';
 import { CpuObservationEndpoint } from 'cpu-monitoring-models';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
 /**
  * service for managing endpoints with functions for adding, editing, 
  * deleting and getting all endpoints
  */
+@Injectable({
+  providedIn: 'root'
+})
 export class EndpoitsService {
 
   api = "http://localhost:3100";
@@ -19,12 +19,14 @@ export class EndpoitsService {
   constructor(private http: HttpClient) {
     this.getObservationEndpoints();
   }
+
   /**
    * send a get request to http://localhost:3100 and get the list of endpoints
    */
   getObservationEndpoints() {
     this.observationEndpointsObs = this.http.get<CpuObservationEndpoint[]>(this.api);
   }
+
   /**
    * send post request to http://localhost:3100 and add the endpoint to the list
    * @param endpoint the given cpu endpoint
@@ -32,6 +34,7 @@ export class EndpoitsService {
   addEndpoint(endpoint: CpuObservationEndpoint) {
     this.observationEndpointsObs = this.http.post<CpuObservationEndpoint[]>(this.api, endpoint);
   }
+
   /**
    * send post request to http://localhost:3100/edit to edit and then update the specified endpoint
    * @param endpoint the given cpu endpoint
@@ -39,6 +42,7 @@ export class EndpoitsService {
   editEndpoint(endpoint: CpuObservationEndpoint) {
     this.observationEndpointsObs = this.http.post<CpuObservationEndpoint[]>(`${this.api}/edit`, endpoint);
   }
+
   /**
    * send a post request to http://localhost:3100/delete to delete the given endpoint
    * @param endpoint the given cpu endpoint

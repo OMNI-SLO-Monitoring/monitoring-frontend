f8b1ae3cce181503da6494429dbb1925cd81f7ef
Jonas Wallmann <jonaswallmann@hotmail.de>
2020-08-25 11:12:53 +0200

    test fix

--- src/app/monitoring-selection/selection-list/selection-list.component.spec.ts
+++ src/app/monitoring-selection/selection-list/selection-list.component.spec.ts
@@ -3,6 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SelectionListComponent } from './selection-list.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SelectionItemComponent } from '../selection-item/selection-item.component';
+import { EditSelectionComponent } from '../edit-selection/edit-selection.component';
+import { MaterialModule } from 'src/app/material.module';
 
 describe('SelectionListComponent', () => {
   let component: SelectionListComponent;
@@ -10,8 +13,8 @@ describe('SelectionListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SelectionListComponent],
-      imports: [MatDialogModule, HttpClientTestingModule],
+      declarations: [SelectionListComponent, SelectionItemComponent, EditSelectionComponent],
+      imports: [MaterialModule, HttpClientTestingModule],
     }).compileComponents();
   }));
 
